26d912aa958facc85d06e09cdfbe2bf81abcfee3
eheredia <edgar.heredia@bci.cl>
2021-04-27 10:45:36 -0400

    Add LastWord in Java

---
+++ Java/LastWord.java
@@ -0,0 +1,43 @@
+/**
+ * Length of Last Word:
+ * Given a string s consists of some words separated by spaces,
+ * return the length of the last word in the string.
+ * If the last word does not exist, return 0.
+ * A word is a maximal substring consisting of non-space characters only.
+ * site: https://leetcode.com/problems/length-of-last-word/
+ * Author: Samuel Heredia. heredia.samuel@gmail.com
+ * Date: 27-04-2021
+ * Note: If you find something in the algorithm that can be improved,
+ * I would like to receive your comments. Thanks
+ */
+public class LastWord {
+
+    public static void main(String arg[]){
+        String test= "Hello Word";
+        System.out.println("Length of last word is : "+ lengthOfLastWord(test));
+    }
+
+    static public int lengthOfLastWord(String s) {
+        int count = 0;
+        if(s == null || s.isEmpty() || s == " "){
+            return 0;
+        }else{
+            int i = 1;
+            boolean band = true;
+            int size = s.length();
+            while(band){
+                if(size == i){
+                    band = false;
+                }else{
+                    if(s.charAt(size-i) != ' '){
+                        count++;
+                    }else{
+                        band = false;
+                    }
+                    i++;
+                }
+            }
+        }
+        return count;
+    }
+}
